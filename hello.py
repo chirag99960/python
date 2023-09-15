c665b95f5f9a9103c8ed1c99f3083e1b2987ddfe
chirag <chiragchandak99960@gmail.com>
2023-09-15 13:37:54 +0530

    This a pyhton file

---
+++ hello.py
@@ -0,0 +1,2 @@
+print("Hello guys! How are you?")
+print("This is the first python file made by Chirag")
---
+++ sum.py
@@ -0,0 +1,5 @@
+print("Enter the two number:-")
+a=eval(input("enter the 1st no:-"))
+b=eval(input("enter the 2nd no:-"))
+
+print("sum of two number is",a+b)
